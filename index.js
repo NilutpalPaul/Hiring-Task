70bdbe75c2f875cdfa5341c964159479e05b4c1e
Nilutpal Paul <85864155+NilutpalPaul@users.noreply.github.com>
2023-04-12 02:29:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+import styles from '@/styles/Home.module.css'; // Import the CSS file
 
-export default function Home() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    async function fetchTasks() {
-      const response = await axios.get('/api/tasks');
-      setTasks(response.data);
-    }
-    fetchTasks();
-  }, []);
-
+const MyComponent = () => {
   return (
-    <div>
-      <h1>Hiring Task Application</h1>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id}>
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
-            <p>{task.dueDate}</p>
-            <button>Complete</button>
-          </li>
-        ))}
-      </ul>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        <img className={styles.image} src="https://cdn.cutshort.io/public/companies/5950dfcf58f0627f77973a12/admybrand-logo" alt="ADmyBRAND logo" /> &emsp; &emsp; &emsp; &emsp; <button className={styles.text} type="button">HIRING TASK</button>
+        
+        <p className={styles.p}>&gt; &gt; &gt; &gt;
+</p>
+      </div>
     </div>
   );
-}
+};
+
+export default MyComponent;
